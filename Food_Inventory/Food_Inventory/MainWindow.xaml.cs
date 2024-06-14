ca241a77fbb43e2100c0d249756402e2a9103c3b
ViktoriyaAuseichyk <ria.bhavanshi@gmail.com>
2024-06-15 00:26:35 +0200

    MainWindow,MonthSelectionWindow

--- Food_Inventory/Food_Inventory/MainWindow.xaml.cs
+++ Food_Inventory/Food_Inventory/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,14 +17,36 @@ using System.Windows.Shapes;
 
 namespace Food_Inventory
 {
+
     public partial class MainWindow : Window
-    {  
+    {
+        static public string CurrentMonth {  get; set; }
+
+        public Button currenPressedtButton;
+        public Grid currentVisibleGrid;
+
+        public Dictionary<Button, Grid> connectingButtonToGrid;
+
         public MainWindow()
         {
             InitializeComponent();
+            currenPressedtButton = MenuPanel_Main_Button;
+            currentVisibleGrid = MainPanel_Grid;
+            connectingButtonToGrid = new Dictionary<Button, Grid>();
+            SettingConnectionsBetweenButtonAndGrid(connectingButtonToGrid);
+            CurrentMonth = "СЕНТЯБРЬ";
+            MainPanel_Date_Month_Button.Content = CurrentMonth;
         }
 
-        
+        private void SettingConnectionsBetweenButtonAndGrid(Dictionary<Button, Grid> connectingButtonToGrid)
+        {
+            connectingButtonToGrid.Add(MenuPanel_Main_Button, MainPanel_Grid);
+            connectingButtonToGrid.Add(MenuPanel_Categories_Button, CategoriesPanel_Grid);
+            connectingButtonToGrid.Add(MenuPanel_Products_Button, ProductsPanel_Grid);
+            connectingButtonToGrid.Add(MenuPanel_Diet_Button, DietPanel_Grid);
+            connectingButtonToGrid.Add(MenuPanel_Limits_Button, LimitsPanel_Grid);
+            connectingButtonToGrid.Add(MenuPanel_Statistics_Button, StatisticsPanel_Grid);
+        }
 
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -54,17 +77,101 @@ namespace Food_Inventory
             else if (this.WindowState == WindowState.Maximized)
             {
                 this.WindowState = WindowState.Normal;
-                screenHeight = 1024;
+                screenHeight = 768;
                 this.Height = screenHeight;
-                screenWidth = 1440;
+                screenWidth = 1080;
                 this.Width = screenWidth;
                 this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
             }
         }
 
         private void MinimiseButton_Click(object sender, RoutedEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
+        }
+
+
+
+        private void CategoriesPanel_List_Search_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (CategoriesPanel_List_Search_Field_Grid.Visibility != Visibility.Visible)
+            {
+                CategoriesPanel_List_Search_Field_Grid.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                CategoriesPanel_List_Search_Field_Grid.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void CategoriesPanel_List_Search_Field_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.CategoriesPanel_List_Search_Field_TextBox.Text != "")
+            {
+                this.CategoriesPanel_List_Search_Field_Label.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                this.CategoriesPanel_List_Search_Field_Label.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void CategoriesPanel_AddCategory_Dish_Field_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.CategoriesPanel_AddCategory_Dish_Field_TextBox.Text != "")
+            {
+                this.CategoriesPanel_AddCategory_Dish_Field_Label.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                this.CategoriesPanel_AddCategory_Dish_Field_Label.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void CategoriesPanel_AddCategory_Diet_Field_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.CategoriesPanel_AddCategory_Diet_Field_TextBox.Text != "")
+            {
+                this.CategoriesPanel_AddCategory_Diet_Field_Label.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                this.CategoriesPanel_AddCategory_Diet_Field_Label.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void MenuPanel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush baseBackgroundColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF4E5D7A"));
+            SolidColorBrush newBackgroundColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF272E3D"));
+
+            currentVisibleGrid.Visibility = Visibility.Hidden;
+            currenPressedtButton.Background = baseBackgroundColor;
+
+            foreach (Button button in connectingButtonToGrid.Keys)
+            {
+                if (button == sender)
+                {
+                    button.Background = newBackgroundColor;
+                    connectingButtonToGrid[button].Visibility = Visibility.Visible;
+                    currentVisibleGrid = connectingButtonToGrid[button];
+                    currenPressedtButton = button;  
+                }
+            }
+        }
+
+        private void MainPanel_DaysOfMonth_LeftArrow_Button_Click(object sender, RoutedEventArgs e)
+        {
+    
+        }
+
+        private void MainPanel_Date_Month_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MonthSelectionWindow monthSelectionWindow = new MonthSelectionWindow();
+            monthSelectionWindow.Owner = this;
+            monthSelectionWindow.ShowDialog();
+            MainPanel_Date_Month_Button.Content = CurrentMonth;
         }
     }
 }
---
+++ Food_Inventory/Food_Inventory/MonthSelectionWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Food_Inventory
+{
+    /// <summary>
+    /// Interaction logic for MonthSelectionWindow.xaml
+    /// </summary>
+    public partial class MonthSelectionWindow : Window
+    {
+        private Dictionary<Button, String> connectingButtonToMonth;
+        private Button currentMonth;
+        private Button selectedMonth;
+
+        public MonthSelectionWindow()
+        {
+            InitializeComponent();
+
+            connectingButtonToMonth = new Dictionary<Button, String>();
+            SettingConnectionBetweenButtonAndMonth(connectingButtonToMonth);
+            currentMonth = MainPanel_MonthsOfYear_September_Button;
+            currentMonth.Style = (Style)FindResource("MainPanel_MonthsOfYear_CurrentMonth_Button");
+            
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void SettingConnectionBetweenButtonAndMonth(Dictionary<Button, String> connectingButtonToMonth)
+        {
+            connectingButtonToMonth.Add(MainPanel_MonthsOfYear_January_Button, "ЯНВАРЬ");
+            connectingButtonToMonth.Add(MainPanel_MonthsOfYear_February_Button, "ФЕВРАЛЬ"); 
+            connectingButtonToMonth.Add(MainPanel_MonthsOfYear_March_Button, "МАРТ");
+            connectingButtonToMonth.Add(MainPanel_MonthsOfYear_April_Button, "АПРЕЛЬ");
+            connectingButtonToMonth.Add(MainPanel_MonthsOfYear_May_Button, "МАЙ");
+            connectingButtonToMonth.Add(MainPanel_MonthsOfYear_June_Button, "ИЮНЬ");
+            connectingButtonToMonth.Add(MainPanel_MonthsOfYear_July_Button, "ИЮЛЬ");
+            connectingButtonToMonth.Add(MainPanel_MonthsOfYear_August_Button, "АВГУСТ");
+            connectingButtonToMonth.Add(MainPanel_MonthsOfYear_September_Button, "СЕНТЯБРЬ");
+            connectingButtonToMonth.Add(MainPanel_MonthsOfYear_October_Button, "ОКТЯБРЬ");
+            connectingButtonToMonth.Add(MainPanel_MonthsOfYear_November_Button, "НОЯБРЬ");
+            connectingButtonToMonth.Add(MainPanel_MonthsOfYear_December_Button, "ДЕКАБРЬ");
+        }
+
+
+        private void WindowControlPanel_Close_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MainPanel_MonthsOfYear_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectedMonth != null) 
+            {
+                selectedMonth.Style = (Style)FindResource("MainPanel_MonthsOfYear_Button");
+            }
+           
+            foreach (Button button in connectingButtonToMonth.Keys)
+            {
+                if (button == sender)
+                {
+                    if (button != currentMonth)
+                    {
+                        button.Style = (Style)FindResource("MainPanel_MonthsOfYear_SelectedMonth_Button");
+                        selectedMonth = button;
+                        MainWindow.CurrentMonth = connectingButtonToMonth[button];
+                        
+                    }    
+                }
+            }
+
+            this.Close();
+        }
+    }
+}
